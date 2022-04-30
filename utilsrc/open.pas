b312dadd4ad774343cd1c4805f4e034c8b582790
Skrine525 <tsaregorodtsev03@mail.ru>
2022-04-30 14:36:26 +1000

    Utils for Git

---
+++ utilsrc/open.pas
@@ -0,0 +1,50 @@
+﻿program pre;
+uses System.IO;
+
+function FileExist(name : string) : boolean;
+var f: text;
+begin
+  assign(f, name);
+  try
+    reset(f);
+    close(f);
+    result := true;
+  except
+    result := false;
+  end;
+end;
+
+var
+  n, filecounter: integer;
+  files: array of string;
+  filename: string;
+
+begin
+  files := Directory.GetFiles('.', '*.greenfoot', SearchOption.TopDirectoryOnly);
+  
+  filecounter := 1;
+  for var i := 0 to length(files)-1 do begin
+    filename := files[i].Split('\')[1];
+    if(filename <> 'project.greenfoot') then begin
+      writeln(filecounter, ': ', filename);
+      inc(filecounter);
+    end;
+  end;
+  
+  if FileExist('project.greenfoot') then begin
+    &File.Delete('project.greenfoot');
+  end;
+  
+  writeln;
+  write('Enter project number: ');
+  readln(n);
+  
+  if not FileExist('project' + n + '.greenfoot') then begin
+    writeln('project', n, '.greenfoot');
+    exit;
+  end;
+  
+  &File.Copy('project' + n + '.greenfoot', 'project.greenfoot');
+  writeln('Opening');
+  Execute('project.greenfoot');
+end.
---
+++ utilsrc/pre-push.pas
@@ -0,0 +1,39 @@
+﻿program pre;
+uses System.IO;
+
+function FileExist(name : string) : boolean;
+var f: text;
+begin
+  assign(f, name);
+  try
+    reset(f);
+    close(f);
+    result := true;
+  except
+    result := false;
+  end;
+end;
+
+var
+  counter: integer;
+  finding: boolean;
+
+begin
+  counter := 1;
+  finding := true;
+  
+  if not FileExist('project.greenfoot') then begin
+    writeln('project.greenfoot not found');
+    exit;
+  end;
+  
+  while finding do
+  begin
+    if FileExist('project' + counter + '.greenfoot') then
+      inc(counter)
+    else
+      finding := false;
+  end;
+  &File.Move('project.greenfoot', 'project' + counter + '.greenfoot');
+  writeln('project.greenfoot -> project', counter, '.greenfoot');
+end.
